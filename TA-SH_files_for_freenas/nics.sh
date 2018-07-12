04a870b821c036f980ecbfff12c2b4824820ec54
Justin O'Connor <oconnor@hkl.hms.harvard.edu>
2018-07-12 16:12:52 -0400

    get all interfaces that are up and send to logs

--- TA-SH_files_for_freenas/nics.sh
+++ TA-SH_files_for_freenas/nics.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
 #Get nic info
-
-sysctl -a | grep dev.em. | sed 's/\:/\ \=/g' | logger
+for i in `ifconfig -ul`; do
+        if [ "$i" != "lo0" ]; then
+                h=`echo \$i | sed 's/[0-9]\{0,10\}\$//g'`
+                j=`echo \$i | awk -F'[^0-9]*' '{print $2}'`
+                sysctl -a | grep dev.$h.$j | sed 's/\:/\ \=/g' | logger
+        fi
+done
